4fb76b9b9ab6654713ad8a4f8fe4cb220f41d3d8
Liza Glazunova <64409376+lglazunova@users.noreply.github.com>
2020-04-28 10:50:48 +0300

    game has been finished

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ namespace SpriteKind {
     export const cat = SpriteKind.create()
     export const LISA = SpriteKind.create()
     export const fish = SpriteKind.create()
+    export const home = SpriteKind.create()
+    export const lllisa = SpriteKind.create()
 }
 namespace myTiles {
     //% blockIdentity=images._tile
@@ -62,11 +64,106 @@ namespace myTiles {
 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
 `
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.home, function (sprite, otherSprite) {
+    mySprite8 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . f f f f . . . f f f . . . . 
+. f 3 3 3 f . . f 3 3 3 f . . . 
+f 3 3 3 3 f f f d 3 3 3 3 f . . 
+f 3 3 3 d d d d d d 3 3 3 f . . 
+f 3 3 d d d d d d d d 3 3 f . . 
+. f d d d d d d d d d d f . . . 
+. . f d f d d d d f d f . . . . 
+. . f f d d 4 4 d d d f . f f . 
+. . . f d d d d d d d f f d f . 
+. . . f d d d d d d d d d d f . 
+. . . f d d d d d d d d d f f . 
+. . . . f d d d f d d d f . . . 
+. . . . . f d d f d d f . . . . 
+`, SpriteKind.lllisa)
+    game.showLongText("вы думали я съел мышь? нет! я довёз её да дому! ты прошёл игру поздравляю!!отойди от дома и высади  мышь", DialogLayout.Bottom)
+    mySprite8.setPosition(143, 57)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.fish, function (sprite, otherSprite) {
     music.magicWand.play()
     game.showLongText("level complete", DialogLayout.Bottom)
     mySprite6.destroy(effects.halo, 200)
     scene.cameraShake(4, 100)
+    tiles.setTilemap(tiles.createTilemap(
+            hex`10001000040a0a0a0a0a0a0a0a0a0a0a0a0a0a06030f0f0f0f0f0f0f0d100d0f0f0f0f09030f0f0f0f0f0d0d0d100d0f0f0f0f09030f0f0f0f0f0d1010100d0f0f0f0f09030f0f0f0f0f0d100d0d0d0f0f0f0f09030f0f0f0f0f0d100d0f0f0f0f0f0f09030f0f0f0f0f0d100d0f0f0f0f0f0f09030f0f0f0f0f0d100d0f0f0f0f0f0f09030f0f0f0f0f0d100d0f0f0f0f0f0f09030f0f0f0f0f0d100d0f0f0f0f0f0f09030f0f0f0f0f0d100d0f0f0f0f0f0f09030f0f0f0f0f0d100d0f0f0f0f0f0f09030f0f0f0f0f0d100d0f0f0f0f0f0f09030d0d0d0d0d0d100d0f0f0f0f0f0f0903101010101010100d0f0f0f0f0f0f0905080808080808080808080808080807`,
+            img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . 2 . 2 . . . . 2 
+2 . . . . . 2 2 2 . 2 . . . . 2 
+2 . . . . . 2 . . . 2 . . . . 2 
+2 . . . . . 2 . 2 2 2 . . . . 2 
+2 . . . . . 2 . 2 . . . . . . 2 
+2 . . . . . 2 . 2 . . . . . . 2 
+2 . . . . . 2 . 2 . . . . . . 2 
+2 . . . . . 2 . 2 . . . . . . 2 
+2 . . . . . 2 . 2 . . . . . . 2 
+2 . . . . . 2 . 2 . . . . . . 2 
+2 . . . . . 2 . 2 . . . . . . 2 
+2 . . . . . 2 . 2 . . . . . . 2 
+2 2 2 2 2 2 2 . 2 . . . . . . 2 
+2 . . . . . . . 2 . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,sprites.castle.tilePath4,sprites.castle.tilePath1,sprites.castle.tilePath7,sprites.castle.tilePath3,sprites.castle.tilePath9,sprites.castle.tilePath8,sprites.castle.tilePath6,sprites.castle.tilePath2,sprites.castle.tileGrass3,sprites.castle.tilePath5,sprites.castle.tileGrass2,sprites.castle.saplingOak,sprites.castle.shrub,sprites.dungeon.darkGroundCenter],
+            TileScale.Sixteen
+        ))
+    mySprite.setPosition(21, 231)
+    mySprite7 = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . 8 a 8 a a 8 a 8 . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . a a a 8 8 8 a a 8 a 8 a a a . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . a a a 8 a a 8 a 8 a a 8 8 8 a a 8 a a a . . . . . . . . . . . . . . 
+. . . . . . . . . . . 8 a a 8 a a 8 8 8 8 a 8 a a 8 a 8 8 8 8 a a 8 a a 8 . . . . . . . . . . . 
+. . . . . . . . 8 8 8 8 a a 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 a a 8 8 8 8 . . . . . . . . 
+. . . . . a a a 8 a a 8 a a 8 8 8 8 a a 8 a 8 a a 8 a 8 a a 8 8 8 8 a a 8 a a 8 a a a . . . . . 
+. . . a a 8 8 8 8 a a 8 a a 8 a a 8 a a 8 8 8 a a 8 8 8 a a 8 a a 8 a a 8 a a 8 8 8 8 a a . . . 
+d c c a a 8 a a 8 a a 8 8 8 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 8 8 8 a a 8 a a 8 a a c c d 
+b c b 8 8 8 a a 8 a a 8 a a 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 a a 8 a a 8 a a 8 8 8 b c b 
+d b b a a 8 a a 8 8 8 8 a a 8 a a 8 8 8 8 a 8 a a 8 a 8 8 8 8 a a 8 a a 8 8 8 8 a a 8 a a b b d 
+d b b a a 8 a a 8 a a 8 a a 8 8 8 8 a a 8 a 8 a a 8 a 8 a a 8 8 8 8 a a 8 a a 8 a a 8 a a b b d 
+d c c a a 8 8 8 8 a a 8 a a 8 a a 8 a a 8 8 8 a a 8 8 8 a a 8 a a 8 a a 8 a a 8 8 8 8 a a c c d 
+b c b a a 8 a a 8 a a 8 8 8 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 8 8 8 a a 8 a a 8 a a b c b 
+d b b 8 8 8 a a 8 a a 8 a a 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 a a 8 a a 8 a a 8 8 8 b b d 
+d b b a a 8 a a 8 8 8 8 a a 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 a a 8 8 8 8 a a 8 a a b b d 
+d c c a a 8 a a 8 a a 8 a a 8 a a 8 8 8 8 a 8 a a 8 a 8 8 8 8 a a 8 a a 8 a a 8 a a 8 a a c c d 
+b c b a a 8 8 8 8 a a 8 a a 8 8 8 8 a a 8 8 8 a a 8 8 8 a a 8 8 8 8 a a 8 a a 8 8 8 8 a a b c b 
+d b b a a 8 a a 8 a a 8 8 8 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 8 8 8 a a 8 a a 8 a a b b d 
+d b b 8 8 8 a a 8 a a 8 a a 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 a a 8 a a 8 a a 8 8 8 b b d 
+d c c a a 8 a a 8 8 8 8 a a 8 a a 8 a a 8 a 8 a a 8 a 8 a a 8 a a 8 a a 8 8 8 8 a a 8 a a c c d 
+b c b a a 8 a a 8 a a 8 a a 8 a a 8 a a 8 8 8 a a 8 8 8 a a 8 a a 8 a a 8 a a 8 a a 8 a a b c b 
+d b b a a 8 8 8 8 a a 8 a a 8 a a 8 8 8 c c b b b b c c 8 8 8 a a 8 a a 8 a a 8 8 8 8 a a b b d 
+d b b a a 8 a a 8 a a 8 a a 8 8 8 c c b b b b b b b b b b c c 8 8 8 a a 8 a a 8 a a 8 a a b b d 
+d c c 8 8 8 a a 8 a a 8 8 8 c c b b b b b c c c c c c b b b b b c c 8 8 8 a a 8 a a 8 8 8 c c d 
+b c b a a 8 a a 8 8 8 c c b b b b b c c b d d d d d d b c c b b b b b c c 8 8 8 a a 8 a a b c b 
+d b b a a 8 a a c c b b b b b c c b d d d d d d d d d d d d b c c b b b b b c c a a 8 a a b b d 
+d b b a a c c b b b b c c c b d d d d d d d d d d d d d d d d d d b c c c b b b b c c a a b b d 
+d c c c b b b b c c c b d d d b c c b b b b b b b b b b b b c c b d d d b c c c b b b b c c c d 
+c c c c c c c c b b b b b b b c b d d d d d d d d d d d d d d b c b b b b b b b c c c c c c c c 
+b d d d d d d d d d d d d b c d d d d d d d d d d d d d d d d d d c b d d d d d d d d d d d d b 
+b b c b d d d d d d d d d c b d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d b c d d d d d d d d d b c b b 
+b b b c c c c c c c c c c c d 1 b b b b b b b b b b b b b b b b 1 d c c c c c c c c c c c b b b 
+b b b b d d d d d d d d d c 1 1 b e e e e e e e e e e e e e e b 1 1 c d d d d d d d d d b b b b 
+b b b 8 a a a a a a a 8 d c 1 b e 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b 1 c d 8 a a a a a a a 8 b b b 
+b b b 8 8 8 8 8 8 8 8 8 d c 1 b e 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b 1 c d 8 8 8 8 8 8 8 8 8 b b b 
+b b b 8 3 3 3 3 3 3 3 8 d c b b f 3 b 3 e f f f f f f e 3 3 b e b b c d 8 3 3 3 3 3 3 3 8 b b b 
+b b b 8 3 f f 3 f f 3 8 d c b b f 3 b f f f f f f f f f f 3 b e b b c d 8 3 f f 3 f f 3 8 b b b 
+b b b 8 3 c c 3 c c 3 8 d c b b f 3 e f f f f f f f f f f e b e b b c d 8 3 c c 3 c c 3 8 b b b 
+b b b 8 3 3 3 3 3 3 3 8 d c b b f 3 e e e e e e e e e e e e b e b b c d 8 3 3 3 3 3 3 3 8 b b b 
+c b b 8 3 f f 3 f f 3 8 d c b b e 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b b c d 8 3 f f 3 f f 3 8 b b c 
+c b b 8 3 c c 3 c c 3 8 d c b b e 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b b c d 8 3 c c 3 c c 3 8 b b c 
+c c b b b b b b b b b b d c b b e 3 b 3 3 b 3 3 b 3 3 f e e e e b b c d b b b b b b b b b b c c 
+. c b b d d d d d d d d d c b b e 3 b 3 3 b 3 3 b 3 3 f f f f e b b c d d d d d d d d d b b c . 
+. . c b d b b b d b b b d c b b f 3 b 3 3 b 3 3 b 3 3 f 3 3 f e b b c d b b b d b b b d b c . . 
+. . . c d b b b d b b b d c b b f 3 b 3 3 b 3 3 b 3 3 b f f e e b b c d b b b d b b b d c . . . 
+. . . . b d d d d d d d d c b b f 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b b c d d d d d d d d b . . . . 
+. . . . . b d b b b d d d c b b f 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b b c d d d b b b d b . . . . . 
+. . . . . . b c c c b b b c b b e 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b b c b b b c c c b . . . . . . 
+`, SpriteKind.home)
+    mySprite7.setPosition(152, 22)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     mySprite2.destroy(effects.spray, 500)
@@ -279,7 +376,7 @@ f 1 1 4 4 4 1 c 4 f 4 4 1 f f f
 . . . . . . f f f f d d d c . . 
 . . . . . . . . . . c c c . . . 
 `, SpriteKind.fish)
-    mySprite6.setPosition(4, 2)
+    mySprite6.setPosition(23, 23)
 })
 controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
     controller.player1.moveSprite(mySprite, 100, 100)
@@ -287,28 +384,31 @@ controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
 let mySprite5: Sprite = null
 let mySprite4: Sprite = null
 let mySprite3: Sprite = null
+let mySprite7: Sprite = null
 let mySprite6: Sprite = null
+let mySprite8: Sprite = null
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
+game.showLongText("glazunov's game представляет: cat and mouse", DialogLayout.Bottom)
 mySprite = sprites.create(img`
-e e e . . . . e e e . . . . 
-c d d c . . c d d c . . . . 
-c b d d f f d d b c . . . . 
-c 3 b d d b d b 3 c . . . . 
-f b 3 d d d d 3 b f . . . . 
-e d d d d d d d d e . . . . 
-e d f d d d d f d e . b f b 
-f d d f d d f d d f . f d f 
-f b d d b b d d 2 f . f d f 
-. f 2 2 2 2 2 2 b b f f d f 
-. f b d d d d d d b b d b f 
-. f d d d d d b d d f f f . 
-. f d f f f d f f d f . . . 
-. f f . . f f . . f f . . . 
+. . . . . . . . . . . . . . 
+d d d . . . . d d d . . . . 
+d 3 3 d . . d 3 3 d . . . . 
+d 3 3 3 d d 3 3 3 d . . . . 
+d 3 3 3 d d 3 3 3 d . . . . 
+d 3 3 d d d d 3 3 d . . . . 
+d d d d d d d d d d . . . . 
+d d f 1 d d f 1 d d . . . . 
+d d f f d d f f d d . d . . 
+d d d d 3 3 d d d d . d . . 
+. 9 5 9 5 9 5 9 5 9 d d . . 
+. d d d d d d d d d d d . . 
+. d d d d d d d d d . . . . 
+. . d d . . . . d d . . . . 
 `, SpriteKind.Player)
 scene.setBackgroundColor(9)
 tiles.setTilemap(tiles.createTilemap(
-            hex`10001000030c0c0c0c0c0c0c0c0c0c0c0c0c0c0609150e0e101010100e0215101002100a0915101315151515151515151510100a09150e130e1002100e0e0e020210020a09151015151515100e0e0e101010100a0913101313131510101515151510100a091610101002100e0e130e101502100a091616161613131313130e101410100a091010101013101010020e021010100a0913131313131313131313131302100a091002100e0e13100e1010100202100a0910100e0e0e1310131313131313100a0910100e020e1010101010101010100a0913131313131313131313131313100a09021010101010020e1010101010100a080d0b0b0b0b0b0b0b0b0b0b0b0b0b0a`,
+            hex`10001000030c0c0c0c0c0c0c0c0c0c0c0c0c0c0609150e0e101010100e0215101002100a0915101315151515151515151510100a09150e130e1002100e0e0e020210020a09151015151515100e0e0e101010100a0913101313131510101515151510100a091610101002100e0e130e101502100a091616161613131313130e101410100a091010101013101010020e021010100a0913131313131313131313131302100a091002100e0e13100e1010100202100a0910100e0e0e1310131313131313100a0910100e020e1010101010101010100a0913131313131313131313131313100a09021010101010020e1010101010100a080b0b0b0b0b0b0b0b0b0b0b0b0b0b07`,
             img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 2 2 . . . . . . . . 2 . . . . 2 
